6d571280e2edbd08d798018009f4f3c73c34aff1
sijibomi <gbemilesanmi@gmail.com>
2023-02-07 13:27:14 +0100

    firsts dots

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#sym link test
+
+# Terminate already running bar instances
+killall -q polybar
+
+int=$(iwgetid | getn 1)
+echo ${int}
+
+sed -i "s/interface = .*/interface = \"$int\"/g" ~/.config/polybar/config
+
+
+# Launch bar1 and bar2
+polybar white &
+#polybar bar2 &
+
+echo "Bars launched..."
